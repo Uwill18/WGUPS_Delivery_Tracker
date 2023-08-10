ab9e3710ae8229887cd0faddede349af89997a7f
UWILL18 <uwill18@wgu.edu>
2023-08-09 23:12:53 -0500

    starting truck and travel data

--- TravelData.py
+++ TravelData.py
@@ -18,3 +18,29 @@ def load_address_data():
         reader = list(csv.reader(f))
         for row in reader[0:]:
             address_data.append(row)
+
+
+load_distance_data()
+load_address_data()
+
+
+# get adressname, and position for comparison
+
+# Method for finding distance between two addresses
+
+
+# https://www.linkedin.com/learning/python-object-oriented-programming/equality-and-comparison?contextUrn=urn%3Ali%3AlyndaLearningPath%3A5f6cf9fe498e1b8929698639&resume=false&u=2045532
+
+def distance_in_between(xpos, ypos):
+    distance = distance_data[xpos][ypos]
+    if distance == '':
+        distance = distance_data[ypos][xpos]
+
+    return float(distance)
+
+
+# Method to get address number from string literal of address
+def address_index(address):
+    for row in address_data:
+        if address in row[2]:
+            return int(row[0])
--- Truck.py
+++ Truck.py
@@ -1,5 +1,8 @@
 #https://www.linkedin.com/learning/python-object-oriented-programming/equality-and-comparison?contextUrn=urn%3Ali%3AlyndaLearningPath%3A5f6cf9fe498e1b8929698639&resume=false&u=2045532
 class Truck:
-    def __init__(self, currentAddress, packages):
+    def __init__(self, currentAddress, pkg_load, pkg_max, avg_mph, tot_miles):
         self.currentAddress = currentAddress
-        self.packages = packages
+        self.pkg_load = pkg_load
+        self.pkg_max = pkg_max
+        self.avg_mph = avg_mph
+        self.tot_miles = tot_miles
